6649679a68b008f104ba1c5dda736b3b226ff7fd
Connor Reed <conman2305@hotmail.com>
2026-03-20 15:06:48 -0400

    Switch to desktop-autostart

--- files/scripts/install-sunshine.sh
+++ files/scripts/install-sunshine.sh
@@ -3,3 +3,6 @@ set -eux
 set -o pipefail
 
 setcap cap_sys_admin+p $(readlink -f $(which sunshine))
+
+mkdir -p /usr/etc/xdg/autostart
+ln -s /usr/share/applications/dev.lizardbyte.app.Sunshine.desktop /usr/etc/xdg/autostart/dev.lizardbyte.app.Sunshine.desktop
