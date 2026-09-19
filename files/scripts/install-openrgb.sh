443ebda8e93424f1684ea4e58c8543589da947b7
Connor Reed <conman2305@hotmail.com>
2026-09-19 11:14:33 -0400

    Disable udev rules

--- files/scripts/install-openrgb.sh
+++ files/scripts/install-openrgb.sh
@@ -3,7 +3,7 @@ set -eu
 set -o pipefail
 
 # Download udev rules file
-wget https://openrgb.org/releases/release_0.9/60-openrgb.rules
+#wget https://openrgb.org/releases/release_0.9/60-openrgb.rules
 
 # Move udev rules file to udev rules directory
-mv 60-openrgb.rules /usr/lib/udev/rules.d
+#mv 60-openrgb.rules /usr/lib/udev/rules.d
